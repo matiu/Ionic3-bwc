8ca87122aac5a06854e7601cf6f00bf0d0bab50c
Matias Alejo Garcia <ematiu@gmail.com>
2017-10-28 09:35:58 -0300

    Working with BWC 6.4!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as BWC from 'bitcore-wallet-client';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public b: any;
+
   constructor(public navCtrl: NavController) {
+console.log('[home.ts.15] Constructor...'); //TODO
+    var client = new BWC({
+      verbose: true,
+      baseUrl: 'https://bws.bitpay.com/bws/api',
+    });
 
+    var self = this;
+
+    console.log('[home.ts.20] creating...'); //TODO
+    client.createWallet("My Wallet", "Irene", 2, 2, {network: 'testnet'}, function(err, secret) {
+      if (err) {
+        console.log('error: ',err);
+        return
+      };
+          console.log('[home.ts.21:client:]',secret); //TODO
+
+      self.b= secret;
+      // Handle err
+      console.log('Wallet Created. Share this secret with your copayers: ' + secret);
+    });
+ 
   }
-
 }
+
